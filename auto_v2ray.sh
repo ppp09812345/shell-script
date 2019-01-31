48ee24b038e909020cb040838ab51f84a5e687eb
TAO <ppp09812345@163.com>
2019-01-31 17:37:12 +0800

    自动安装v2ray代理

---
+++ auto_v2ray.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd
+
+wget https://install.direct/go.sh 2>/dev/null
+
+sudo chmod 755 go.sh
+
+sudo ./go.sh 2>/dev/null
+
+cd /etc/v2ray
+
+sudo mv config.json config.json.bak
+
+wget https://raw.githubusercontent.com/ppp09812345/config/master/config.json 2>/dev/null
+
+sudo chmod 755 config.json
+
+sudo systemctl start v2ray 2>/dev/null
+
+v2ray_process=`ps aux |grep $yoyo |grep -v grep | awk -F ' ' '{print $1"\t"$11}' | wc -l`
+
+stop_process=0
+
+if [$v2ray_process -gt $stop_process]
+then
+    echo "something wrong!!!!"
+else
+    echo "ok!"
+fi
